70ce3e78f700e6f754849e616fa472a3ee3b8743
Yuragranovsky <yuragranovsky@gmail.com>
2021-10-31 21:09:48 +0200

    hw #8 initial commit

---
+++ src/telran/util/LinkedList.java
@@ -0,0 +1,160 @@
+package telran.util;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Predicate;
+
+public class LinkedList<T> implements List<T> {
+	private int size;
+
+	private static class Node<T> {
+		T obj;
+		Node<T> next;
+		Node<T> prev;
+
+		Node(T obj) {
+			this.obj = obj;
+		}
+	}
+
+	private Node<T> head; // reference to the first element
+	private Node<T> tail; // reference to the last element
+
+	@Override
+	public void add(T element) {
+		Node<T> newNode = new Node<>(element);
+		if (head == null) {
+			head = tail = newNode;
+		} else {
+			tail.next = newNode;
+			newNode.prev = tail;
+			tail = newNode;
+		}
+		size++;
+
+	}
+	private Node<T> getNode(int index) {
+		Node<T> res = null;
+		if(isValidIndex(index)) {
+			res = index <= size / 2 ? getNodefromLeft(index) : getNodeFromRight(index);
+		}
+		return res;
+	}
+
+	private Node<T> getNodeFromRight(int index) {
+		Node<T> current = tail;
+		int ind = size - 1;
+		while(ind != index) {
+			ind--;
+			current = current.prev;
+		}
+		return current;
+	}
+	private Node<T> getNodefromLeft(int index) {
+		Node<T> current = head;
+		int ind = 0;
+		while(ind != index) {
+			ind++;
+			current = current.next;
+		}
+		return current;
+	}
+	private boolean isValidIndex(int index) {
+		
+		return index >=0 && index < size;
+	}
+	@Override
+	public boolean add(int index, T element) {
+		boolean res = false;
+		if (index == size) {
+			add(element);
+			res = true;
+		} else if (isValidIndex(index)) {
+			res = true;
+			Node<T> newNode = new Node<>(element);
+			if (index == 0) {
+				addHead(newNode);
+			} else {
+				addMiddle(newNode, index);
+			}
+			size++;
+		}
+		return res;
+	}
+
+	private void addMiddle(Node<T> newNode, int index) {
+		Node<T> nodeAfter = getNode(index);
+		newNode.next = nodeAfter;
+		newNode.prev = nodeAfter.prev;
+		//nodeAfter.prev => reference to the old previous element
+		nodeAfter.prev.next = newNode;
+		nodeAfter.prev = newNode;
+		
+	}
+	private void addHead(Node<T> newNode) {
+		newNode.next = head;
+		head.prev = newNode;
+		head = newNode;
+		
+	}
+	@Override
+	public int size() {
+
+		return size;
+	}
+
+	@Override
+	public T get(int index) {
+		T res = null;
+		Node<T> resNode = getNode(index);
+		if (resNode != null) {
+			res = resNode.obj;
+		}
+		return res;
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Predicate<T> predicate) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndexOf(Predicate<T> predicate) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean removeIf(Predicate<T> predicate) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void sort(Comparator<T> comp) {
+		T[] array = listToArray();
+		Arrays.sort(array, comp);
+		fillListFromArray(array);
+		
+
+	}
+	private T[] listToArray() {
+		//TODO
+		//creates array of T objects
+		//passes over whole list and fills the array
+		//sorting filled array
+		return null;
+	}
+	private void fillListFromArray(T[] array) {
+		//TODO
+		//passes over whole list and fills elements from index=0 to index=size - 1 
+	}
+
+}
